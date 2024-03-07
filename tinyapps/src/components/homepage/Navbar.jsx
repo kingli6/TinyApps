a4abfcfd5e035f0192b698c0ac1ac34230fe1229
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-03-07 19:03:23 +0100

    styling css

--- tinyapps/src/components/homepage/Navbar.jsx
+++ tinyapps/src/components/homepage/Navbar.jsx
@@ -1,21 +1,44 @@
 import { Link } from 'react-router-dom';
-import headerStyles from "../basicsReact/header.module.css"
+import styles from "../basicsReact/header.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 const Navbar = () => {
 
   return (
-    <nav className={headerStyles.headerContainer}>
-      
+  <div className={styles.navbody}>
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
         <Link to="/">
-        <div className={headerStyles.nav}>Tiny Apps</div>
+        <div className={styles.logo}>Tiny Apps</div>
         </Link>
-    
-        <div className={headerStyles.nav}>
+        <ul className={styles.links}>
+          <div className={styles.contact}>
+            <Link to="/contact" >Contact</Link>
+          </div>
+        </ul>
+        <div className={styles.toggle_btn}>
+          <i className={styles.logoBar}><FontAwesomeIcon icon={faBars} /></i>
+        </div>
+      </nav>
+      {/* // adding two css classes ={`${styles.dropdown_menu} open`}*/}
+      <div className={styles.dropdown_menu}>
+        <div className={styles.contact}>
           <Link to="/contact" >
             Contact
           </Link>
         </div>
-      
-    </nav>
+      </div>
+    </header>
+    {/* <sript>
+      const toggleBtn = document.querySelector('.toggle_btn')
+      const toggleBtnIcon = document.querySelector('.toggle_btn div')
+      const dropdownMenu = document.querySelector('.dropdown_menu')
+
+      toggleBtn.onclick = function () {
+        dropDownMenu.classList.toggle('open');
+      }
+    </sript> */}
+  </div>
   );
 };
 
--- tinyapps/src/components/testPage1/TestPage1.jsx
+++ tinyapps/src/components/testPage1/TestPage1.jsx
@@ -1,13 +1,35 @@
 import styles from './testPage.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 const TestPage1 = () => {
   return (
-    <div>
-      <h2>TestPage1</h2>
-      <div className={styles.parent}>parent
-        <div className={styles.child}>
-          <p>child</p>
+    <div className={styles.body}>
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}><a href='#'>Tiny Apps</a></div>
+        <ul className={styles.links}>
+          <li><a href="hero">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="services">Services</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+        <a href="" className={styles.action_btn}>Get Started</a>
+        <div className={styles.toggle_btn}>
+          <i className={styles.logos}><FontAwesomeIcon icon={faBars} /></i>
         </div>
+      </nav>
+    </header>
+    <positioning>
+      <div className={styles.parent}>
+        <p>A line of text</p>
+        <div className={styles.child}></div>
+        <p>More text</p>
       </div>
+    </positioning>
+      
+      <footer>
+
+      </footer>
     </div>
   )
 }
