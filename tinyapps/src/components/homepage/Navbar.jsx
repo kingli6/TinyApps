8bc73c883cfce9aa7eb31220a9a22e90dc32f69e
Joanthan Fisher <jonathan.fshr@gmail.com>
2024-03-08 13:56:30 +0100

    will get rid of this

--- tinyapps/src/components/homepage/Navbar.jsx
+++ tinyapps/src/components/homepage/Navbar.jsx
@@ -1,44 +1,48 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import styles from "../basicsReact/header.module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import styles from '../basicsReact/header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+ 
+  const toggleDropdown = () => {
+    console.log("Toggling dropdown"); // Add console.log statement
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
   return (
-  <div className={styles.navbody}>
-    <header className={styles.header}>
-      <nav className={styles.navbar}>
-        <Link to="/">
-        <div className={styles.logo}>Tiny Apps</div>
-        </Link>
-        <ul className={styles.links}>
-          <div className={styles.contact}>
-            <Link to="/contact" >Contact</Link>
-          </div>
-        </ul>
-        <div className={styles.toggle_btn}>
-          <i className={styles.logoBar}><FontAwesomeIcon icon={faBars} /></i>
-        </div>
-      </nav>
-      {/* // adding two css classes ={`${styles.dropdown_menu} open`}*/}
-      <div className={styles.dropdown_menu}>
-        <div className={styles.contact}>
-          <Link to="/contact" >
-            Contact
+    <div className={styles.navbody}>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <Link to="/">
+            <div className={styles.logo}>Tiny Apps</div>
           </Link>
-        </div>
-      </div>
-    </header>
-    {/* <sript>
-      const toggleBtn = document.querySelector('.toggle_btn')
-      const toggleBtnIcon = document.querySelector('.toggle_btn div')
-      const dropdownMenu = document.querySelector('.dropdown_menu')
-
-      toggleBtn.onclick = function () {
-        dropDownMenu.classList.toggle('open');
-      }
-    </sript> */}
-  </div>
+          <ul className={styles.links}>
+            <div className={styles.contact}>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </ul>
+          <div
+            className={styles.toggle_btn}
+            onClick={toggleDropdown}
+            aria-label="Toggle Menu"
+          >
+            <i className={styles.logoBar}>
+              <FontAwesomeIcon icon={faBars} />
+            </i>
+          </div>
+        </nav>
+        {isDropdownOpen && (
+          <div className={styles.dropdown_menu}>
+            <div className={styles.contact}>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </div>
+        )}
+      </header>
+    </div>
   );
 };
 
